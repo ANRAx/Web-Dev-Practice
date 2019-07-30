dfaa64aec0878b03f68ebade5b1e7f52a5176fb5
ANRAx <ramdassaj@gmail.com>
2019-07-30 11:53:36 -0400

    add initial files for DOM practice

---
+++ CSS -My Site/prac.js
@@ -0,0 +1,22 @@
+var output = [];
+var count = 1;
+
+function fizzbuzz() {
+  output.push(count);
+  count++;
+
+  console.log(output);
+}
+
+var numberOfBottles = 99
+while (numberOfBottles >= 0) {
+    var bottleWord = "bottle";
+    if (numberOfBottles === 1) {
+      bottleWord = "bottles";
+    } 
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall");
+    console.log(numberOfBottles + " " + bottleWord + " of beer,");
+    console.log("Take one down, pass it around,");
+	numberOfBottles--;
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall.");
+}
